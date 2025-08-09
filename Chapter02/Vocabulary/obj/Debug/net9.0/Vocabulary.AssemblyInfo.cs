2c8bf962ffc1b1cab7c75cf7994f7c734264b5b7
Santiago <santigs211@gmail.com>
2025-08-09 20:13:22 -0300

    c

--- Chapter02/Vocabulary/obj/Debug/net9.0/Vocabulary.AssemblyInfo.cs
+++ Chapter02/Vocabulary/obj/Debug/net9.0/Vocabulary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vocabulary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+433a461e2ded3dbfcc238669afaf6fd5196ead1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad7caf01eb82483400df2cfa85a2bbb39500eba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vocabulary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vocabulary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
