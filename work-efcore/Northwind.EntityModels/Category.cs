dcee8a772ea6a51fe791029458c865bb3b9d06ff
Santiago <santigs211@gmail.com>
2025-08-09 20:30:25 -0300

    changes

--- work-efcore/Northwind.EntityModels/Category.cs
+++ work-efcore/Northwind.EntityModels/Category.cs
@@ -3,5 +3,11 @@ namespace Northwind.EntityModels;
 
 public class Category
 {
-    
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; } = null!;
+
+    [Column(TypeName = "ntext")]
+    public string? Description { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
 }
--- work-efcore/Northwind.EntityModels/Product.cs
+++ work-efcore/Northwind.EntityModels/Product.cs
@@ -3,5 +3,16 @@ namespace Northwind.EntityModels;
 
 public class Product
 {
-    
+    public int ProductId { get; set; }
+    [Required]
+    [StringLenght(40)]
+    public string ProductName { get; set; } = null!;
+
+    [Column("UnitPrice", TypeName = "money")]
+    public decimal? Cost { get; set; }
+    [Column("UnitsInStock")]
+    public short Stock { get; set; }
+    public bool Discontinued { get; set; }
+    public int CategoryId { get; set; }
+    public virtual Category Category { get; set; } = null!;
 }
